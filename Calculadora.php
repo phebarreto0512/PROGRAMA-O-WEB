a897779a5ab47ad43d56f0e7e8dbae2cec8f1a4c
phebarreto0512 <134434392+phebarreto0512@users.noreply.github.com>
2024-04-16 21:46:38 -0300

    Update Calculadora.php

--- Calculadora.php
+++ Calculadora.php
@@ -112,53 +112,210 @@ class CalculadoraImc
                     return 'Adequado ou Eutrófico';
                 }
             }
-     } elseif ($idade < 10) {
-            return 'necessita ser acima de 10 anos';
+     } elseif ($idade < 13) {
+            return 'necessita ser acima de 13 anos';
         } else {
-            if ($sexo == 'Feminino' && $idade == 10) {
-                if ($imc < 14.23) {
+            if ($sexo == 'Feminino' && $idade == 13) {
+                if ($imc < 15.36) {
                     return 'Baixo Peso';
-                } elseif ($imc > 23.20) {
+                } elseif ($imc > 27.07) {
                     return 'Sobrepeso';
                 } else {
                     return 'Adequado ou Eutrófico';
                 }
             }
-     } elseif ($idade < 10) {
-            return 'necessita ser acima de 10 anos';
+     } elseif ($idade < 14) {
+            return 'necessita ser acima de 14 anos';
         } else {
-            if ($sexo == 'Feminino' && $idade == 10) {
-                if ($imc < 14.23) {
+            if ($sexo == 'Feminino' && $idade == 14) {
+                if ($imc < 15.67) {
                     return 'Baixo Peso';
-                } elseif ($imc > 23.20) {
+                } elseif ($imc > 27.97) {
                     return 'Sobrepeso';
                 } else {
                     return 'Adequado ou Eutrófico';
                 }
             }
-     } elseif ($idade < 10) {
-            return 'necessita ser acima de 10 anos';
+     } elseif ($idade < 15) {
+            return 'necessita ser acima de 15 anos';
         } else {
-            if ($sexo == 'Feminino' && $idade == 10) {
-                if ($imc < 14.23) {
+            if ($sexo == 'Feminino' && $idade == 15
+    ) {
+                if ($imc < 16.01) {
                     return 'Baixo Peso';
-                } elseif ($imc > 23.20) {
+                } elseif ($imc > 28.51) {
                     return 'Sobrepeso';
                 } else {
                     return 'Adequado ou Eutrófico';
                 }
             }
-     } elseif ($idade < 10) {
-            return 'necessita ser acima de 10 anos';
+     } elseif ($idade < 16) {
+            return 'necessita ser acima de 16anos';
         } else {
-            if ($sexo == 'Feminino' && $idade == 10) {
-                if ($imc < 14.23) {
+            if ($sexo == 'Feminino' && $idade == 16) {
+                if ($imc < 16.37) {
                     return 'Baixo Peso';
-                } elseif ($imc > 23.20) {
+                } elseif ($imc > 29.10) {
                     return 'Sobrepeso';
                 } else {
                     return 'Adequado ou Eutrófico';
                 }
             }
     
-   
+   } elseif ($idade < 17) {
+            return 'necessita ser acima de 17 anos';
+        } else {
+            if ($sexo == 'Feminino' && $idade == 17) {
+                if ($imc < 16.59) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 29.72) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+                    } 
+                    elseif ($idade < 18) {
+            return 'necessita ser acima de 18anos';
+        } else {
+            if ($sexo == 'Feminino' && $idade == 18) {
+                if ($imc < 16.71) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 30,22) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+} elseif ($idade < 19) {
+            return 'necessita ser acima de 19anos';
+        } else {
+            if ($sexo == 'Feminino' && $idade == 19) {
+                if ($imc < 16.87) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 30.72) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+
+
+masculino 
+} elseif ($idade < 10) {
+            return 'necessita ser acima de 10 anos'; 
+        } else {
+            if ($sexo == 'masculino' && $idade == 10) { 
+                if ($imc < 14.42) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 22.60) { 
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico'; 
+            }
+    
+} elseif ($idade < 11) {
+            return 'necessita ser acima de 11 anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 12) {
+                if ($imc < 14.83) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 23.70) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+            }
+     } elseif ($idade < 12) {
+            return 'necessita ser acima de 12 anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 12) {
+                if ($imc < 15.24) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 24.89) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+            }
+     } elseif ($idade < 13) {
+            return 'necessita ser acima de 13 anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 13) {
+                if ($imc < 15.73) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 25.93) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+            }
+     } elseif ($idade < 14) {
+            return 'necessita ser acima de 14 anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 14) {
+                if ($imc < 16.18) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 26.93) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+            }
+     } elseif ($idade < 15) {
+            return 'necessita ser acima de 15 anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 15
+    ) {
+                if ($imc < 16.59) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 27.75) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+            }
+     } elseif ($idade < 16) {
+            return 'necessita ser acima de 16anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 16) {
+                if ($imc < 16.98) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 28,61) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+            }
+    
+   } elseif ($idade < 17) {
+            return 'necessita ser acima de 17 anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 17) {
+                if ($imc < 17.29) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 29.49) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+                    } 
+                    elseif ($idade < 18) {
+            return 'necessita ser acima de 18anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 18) {
+                if ($imc < 17.50) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 30,33) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
+} elseif ($idade < 19) {
+            return 'necessita ser acima de 19anos';
+        } else {
+            if ($sexo == 'Masculino' && $idade == 19) {
+                if ($imc < 17.67) {
+                    return 'Baixo Peso';
+                } elseif ($imc > 31.07) {
+                    return 'Sobrepeso';
+                } else {
+                    return 'Adequado ou Eutrófico';
+                }
